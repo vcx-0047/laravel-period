4671f9cef6a7a1402ce624ab499ec88987ac5a35
vcx-0047 <167695472+vcx-0047@users.noreply.github.com>
2025-08-07 19:49:17 +0100

    Create migrate.blueprint.php

---
+++ migrate.blueprint.php
@@ -0,0 +1,7 @@
+public function period()
+    {
+        return new Collection([
+            $this->timestamp('period_start')->nullable(),
+            $this->timestamp('period_end')->nullable(),
+        ]);
+    }
